e1d5862712dbe4df7d5a3bd862eea6473c263f36
nelfarna <elfarnadia@gmail.com>
2015-04-25 01:23:30 -0500

    Reorganized code and improved findNode

--- js/grid.js
+++ js/grid.js
@@ -1,61 +1,60 @@
+(function(exports) {
+	
+	function GridNode (x, y, legal) {
+		this.parent = null;
+		this.legal = legal;
+		this.f = 0;
+		this.g = 0;
+		this.h = 0;
 
-function Grid (width, height) {
-	this.height = height;
-	this.width = width;
-    this.grid;
-}
-
-Grid.prototype.generate = function() {
-	var grid = [];
-	for(var i = 0; i < this.height; i++) {
-		grid[i] = [];
-		for(var j = 0; j < this.width; j++) {
-			grid[i][j] = new GridNode(i, j, Boolean((i*j) % 7 !== 0 ? Math.floor(Math.random() * 200) % 2 : 1));
+		this.pos = {
+			x: x,
+			y: y
 		}
 	}
-	this.grid = grid;
-};
 
-Grid.prototype.findRandomLegalNode = function() {
-	var found = false;
-	var x, y, node;
-	while(!found) {
-		x = Math.floor(Math.random() * this.width);
-		y = Math.floor(Math.random() * this.height);
-		node = this.grid[x][y];
-		if (node.legal) found = true;
+	function Grid (numWidthCells, numHeightCells) {
+		this.numWidthCells = numWidthCells;					// number of cells along width
+		this.numHeightCells = numHeightCells;				// number of cells along height
+
+		// The grid is divided into 16 regions, like street blocks.
+		// The blocks will have a mix of legal and illegal cells (randomly generated)
+		this.blockDepth = Math.floor(numWidthCells / 4);	
+		this.cells = [];
 	}
-	return node;
-};
 
-Grid.prototype.findNode = function(x, y) {
-	var node;
-	for(var j = 0; j < this.grid.length; j++) {
-		for (var i = 0; i < this.grid[0].length; i++) {
+	exports.Grid = Grid;
 
-			if(this.grid[i][j].pos.x === x 
-				&& this.grid[i][j].pos.y === y
-				&& this.grid[i][j].legal) {
-				node = this.grid[i][j];
+	Grid.prototype = {
+		generate: function() {
+			var g = [];
+			for(var i = 0; i < this.numHeightCells; i++) {
+				g[i] = [];
+				for(var j = 0; j < this.numWidthCells; j++) {
+					// if inside block region, then randomly generate legal value, else set to true
+					g[i][j] = new GridNode(i, j, (i*j) % this.blockDepth !== 0 ? Boolean(Math.floor(Math.random() * 200) % 2) : true);
+				}
+			}
+			this.cells = g;
+		},
+		getRandomLegalNode: function() {
+			var found = false;
+			var x, y, node;
+			while(!found) {
+				x = Math.floor(Math.random() * this.numWidthCells);
+				y = Math.floor(Math.random() * this.numHeightCells);
+				node = this.cells[x][y];
+				if (node.legal) found = true;
+			}
+			return node;
+		},
+		findNode: function(x, y) {
+			var node = this.cells[x][y];
+			if(node.pos.x === x && node.pos.y === y && node.legal) {
+				return node;
 			}
 		}
-	}
-	return node;
-};
+	};
 
-
-function GridNode (x, y, legal) {
-	this.parent = null;
-	this.legal = legal;
-	this.f = 0;
-	this.g = 0;
-	this.h = 0;
-
-	this.pos = {
-		x: x,
-		y: y
-	}
-};
-
-
+})(typeof exports === 'undefined' ? this : exports);
 
